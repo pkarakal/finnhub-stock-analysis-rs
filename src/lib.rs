90c5dc7124039252fd5e929fecda36e65babe02d
Pavlos Karakalidis <pavloc.kara@outlook.com>
2022-07-25 23:41:10 +0300

    feat(candlestick): calculate rolling candlestick data each minute

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 pub mod cli;
-use std::{fs::{File, OpenOptions, create_dir_all}, io::{Error}, path::{Path, PathBuf}, fmt::format, io};
-use std::io::Write;
+pub mod stock_handle;
+pub mod utils;
+pub mod candlestick;
+pub mod mean;
+use std::{fs::{File, OpenOptions, create_dir_all}, path::{PathBuf}, io};
 use serde::{Deserialize, Serialize};
 use chrono::{DateTime, NaiveDateTime, Utc, serde::{ts_milliseconds}};
-use csv;
 use csv::StringRecord;
 
 
@@ -42,17 +44,39 @@ pub struct Response<'a> {
     pub transaction_data: Vec<TickerInfo>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(tag = "type", rename_all = "lowercase")]
-pub enum WsMessage {
-    Error {
-        #[serde(rename = "msg")]
-        message: String,
-    },
-    Ping,
-    Response,
+#[derive(Deserialize, Debug, Serialize)]
+pub struct Ping<'a> {
+    #[serde(rename = "type")]
+    pub action_type: &'a str
 }
 
+#[derive(Deserialize, Debug, Serialize)]
+pub struct WsError<'a> {
+    #[serde(rename = "msg")]
+    pub message: &'a str
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged, rename_all = "lowercase")]
+pub enum WsMessage<'a> {
+    #[serde(borrow = "'a")]
+    Response(Response<'a>),
+    #[serde(borrow = "'a")]
+    Error(WsError<'a>),
+    #[serde(borrow = "'a")]
+    Ping(Ping<'a>)
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct RollingData {
+    pub symbol: String,
+    pub price: f64,
+    #[serde(with = "ts_milliseconds")]
+    pub timestamp: DateTime<Utc>,
+    #[serde(with = "ts_milliseconds")]
+    pub write_timestamp: DateTime<Utc>
+}
 
 impl TickerInfo {
     fn default() -> Self {
@@ -75,17 +99,13 @@ impl TickerInfo {
         }
     }
 
-    pub fn write_to_disk(&self, path: &PathBuf) {
-        let file = self.check_file_exists(path);
-        if self.check_file_empty(&file) {
-            self.write_headers(&file);
-        }
-        let mut writer = csv::WriterBuilder::new().has_headers(true).from_writer(&file);
+    pub fn write_to_disk(&self, file: &File) {
+        let mut writer = csv::WriterBuilder::new().has_headers(true).from_writer(file);
         writer.serialize(self.vectorize()).unwrap();
         writer.flush().unwrap();
     }
 
-    fn check_file_empty(&self, file: &File) -> bool {
+    pub fn check_file_empty(&self, file: &File) -> bool {
         // has headers has been set to false as it will skip the first record
         let mut reader = csv::ReaderBuilder::new().has_headers(false).from_reader(file);
         let mut rec = StringRecord::new();
@@ -111,14 +131,13 @@ impl TickerInfo {
                         .unwrap();
                     self.write_headers(&f);
                     f
-                },
-                _ => panic!("Problem opening the file" )
-
+                }
+                _ => panic!("Problem opening the file")
             }
         }
     }
 
-    fn write_headers(&self, f: &File) {
+    pub fn write_headers(&self, f: &File) {
         let mut writer = csv::WriterBuilder::new().has_headers(true).from_writer(f);
         writer.serialize(self.get_headers()).unwrap();
         writer.flush().unwrap();
@@ -129,28 +148,28 @@ impl CSVAble for TickerInfo {
     fn vectorize(&self) -> Vec<String> {
         return vec![self.symbol.clone(),
                     self.price.to_string(),
-                    self.time.timestamp_nanos().to_string(),
-                    Utc::now().timestamp_nanos().to_string(),
+                    self.time.timestamp_millis().to_string(),
+                    Utc::now().timestamp_millis().to_string(),
         ];
     }
     fn get_headers(&self) -> Vec<String> {
         return vec!["Symbol".to_string(),
                     "Price".to_string(),
                     "Timestamp".to_string(),
-                    "Write Timestamp".to_string()];
+                    "WriteTimestamp".to_string()];
     }
 }
 
-impl <'a> Response<'a> {
-    pub fn default() -> Self{
-        Response{
+impl<'a> Response<'a> {
+    pub fn default() -> Self {
+        Response {
             transaction_type: "trade",
             transaction_data: vec![],
         }
     }
 }
 
-impl <'a> SubscribeInfo<'a> {
+impl<'a> SubscribeInfo<'a> {
     pub fn new(symbol: &'a str) -> Self {
         SubscribeInfo {
             message_type: "subscribe",
